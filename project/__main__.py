90207d5c3265108a642c1c8a2ac76f86fed1568d
Daniel Leal <cosaleal@gmail.com>
2022-11-30 22:31:45 -0300

    Initial working structure completed

---
+++ project/__main__.py
@@ -0,0 +1,4 @@
+from project.constants import MODEL_NAME, OPENAI_API_KEY
+
+print(OPENAI_API_KEY)
+print(MODEL_NAME)
---
+++ project/constants.py
@@ -0,0 +1,4 @@
+import os
+
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+MODEL_NAME = os.getenv("MODEL_NAME", "text-davinci-003")
